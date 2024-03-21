3557d27f79893d8576b6f684248af9239eedcaaf
Mohammed Bounoua <mohammedbounoua1995@gmail.com>
2024-03-21 12:44:28 +0100

    Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,32 +1,90 @@
 // Variables //
 
 const gallery = document.querySelector(".gallery")
+const portfolio = document.getElementById("portfolio")
 
-//importation tableau works //
+// recuperation du tableau works //
 async function getworks(){
-const reponse = await fetch("http://localhost:5678/api/works");
-return await reponse.json()
+    const reponse = await fetch("http://localhost:5678/api/works");
+    return await reponse.json()
 }
 getworks()
 
+// recuperation des categories //
+async function getCategories(){
+    const reponse = await fetch("http://localhost:5678/api/categories");
+    return await reponse.json()
+}
+getCategories()
+
 // afficher les works dans le DOM//
+
 async function afficherWorks (){
     const arrayWorks = await getworks()
-    arrayWorks.forEach(work => {
-        // creer les elements du DOM // 
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
-        // preciser l'emplacement des elements crées //
-        gallery.appendChild(figure);
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        // ajouter le contenu de chaque element //
-        img.src = work.imageUrl
-        figcaption.textContent = work.title    
-     });     
+    arrayWorks.forEach((work) => {  
+        createWorks(work)
+     });          
 }
 afficherWorks()
 
+function createWorks(work){
+ // creer les elements du DOM // 
+ const figure = document.createElement("figure");
+ const img = document.createElement("img");
+ const figcaption = document.createElement("figcaption");
+ // ajouter le contenu de chaque element //
+ img.src = work.imageUrl
+ figcaption.textContent = work.title
+ // preciser l'emplacement des elements crées //
+ gallery.appendChild(figure);
+ figure.appendChild(img);
+ figure.appendChild(figcaption);
+    
+}
 
+//afficher les boutons filtres//
 
+async function createBtn (){
+    const arrayCategories = await getCategories()
+    arrayCategories.unshift({id:0 , name:"Tous"}); //ajout d'un nouvel element au tableau des categories
+
+    const containerFiltres = document.createElement("div")
+    containerFiltres.classList.add("container-filtres")
+    portfolio.insertBefore (containerFiltres , gallery)
+   
+    arrayCategories.forEach((categorie) => {  
+        const btn = document.createElement("button")
+        btn.classList.add("buttons-filtres")
+        btn.id = categorie.id
+        btn.textContent = categorie.name
+        containerFiltres.appendChild(btn)
+     });          
+}
+createBtn()
+
+// filtrer les works par categorie //
+
+async function filtrerWorks () {
+    const arrayWorks = await getworks()
+    const buttons = document.querySelectorAll(".buttons-filtres")
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            buttons.forEach(btn => {
+                btn.classList.remove("active");
+            });
+            button.classList.add("active")
+            const btnId = e.target.id  
+            gallery.innerHTML = ""  
+            if (btnId !== "0") {
+                const worksFiltres = arrayWorks.filter((work) => {
+                    return work.categoryId == btnId
+                })
+                worksFiltres.forEach(work => {
+                    createWorks(work)
+                })
+            }else 
+            afficherWorks()           
+        })
+    });
+}
+filtrerWorks()
